7f446740e87c16965ba89047a309034b6c61b72b
estheroche <estheraladioche569@gmail.com>
2023-06-08 23:32:12 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+/*let js = 'amazing';
+        if (js === 'amazing')  alert ('javaScript is FUN!');
+        43 + 3 -4;
+        console.log( 43 + 3 -4);
+        console.log("esther");
+        console.log(43);
+
+        let firstName = "Esther";
+        console.log(firstName);
+        console.log(firstName);
+
+        //variable name convention
+
+        let esther_gold = "EG";
+        let $function = 34;
+        let dollar = 345;
+        let PI =3.4;
+
+        let myFirstJob = 'Programmer';
+        let myCurrentJob ='teacher';
+
+        let job1 = 'Programmer';
+        let job2 = 'Teacher';
+        console.log(myFirstJob);
+        
+
+     let javascriptIsFun = true;
+       console.log(javascriptIsFun);
+       
+
+       //BOOLEN DATA TYPE
+       
+       console.log(typeof true);
+       console.log(typeof javascriptIsFun);
+       console.log(typeof 34);
+       console.log(typeof 'esther');
+
+       //dynamic typing, changing from a boolen to string to define a varible
+// STRING DATA TYPE
+
+       javascriptIsFun = 'YES!';
+       console.log(javascriptIsFun);
+       console.log(typeof javascriptIsFun);
+
+       //BIGINT, UNDEFINE, SYMBOL ARE OTHER DATA TYPES
+
+       let year;
+       console.log(year);
+       console.log(typeof year);
+       //DYNAMIC TYPING
+
+       year = 1991;
+       console.log(typeof year);
+
+//OPERATORE AND TYPES
+//MATHEMATICAL OPERATOR
+const now = 2034;
+const ageEsther = now - 2000;
+const ageGold = now - 2001;
+console.log(ageEsther, ageGold );
+
+console.log(ageEsther * 2, ageEsther / 10, 2 ** 3);
+    // 2 ** 3 means 2*2*2 
+    
+    // to concatinate two or more strings with plus sign 
+
+*/
+
+const firstName = 'esther';
+const lastName = 'oche';
+console.log(firstName + lastName);
